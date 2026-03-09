0001353feb67f8f4bb5ac8d7f53c9dd43794162e
KevinDaCruz <kevindc39@hotmail.com>
2026-03-09 16:53:21 +0100

    Activité 8 fix : Ajout de la structure HTML pour la vue 404

--- resources/views/errors/not-found.blade.php
+++ resources/views/errors/not-found.blade.php
@@ -1,1 +1,15 @@
-<h1>Page non trouvée</h1>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erreur 404</title>
+</head>
+
+<body>
+    <h1>Page non trouvée</h1>
+    <p>La page que vous recherchez est introuvable.</p>
+</body>
+
+</html>
