a11fec367e4ae543dbb55b3303ed27423b9537cd
KevinDaCruz <kevindc39@hotmail.com>
2026-03-10 10:07:38 +0100

    Activité 9 : Réorganisation des vues avec un modèle principal

--- resources/views/errors/not-found.blade.php
+++ resources/views/errors/not-found.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Erreur 404</title>
-</head>
-
-<body>
-    <h1>Page non trouvée</h1>
-    <p>La page que vous recherchez est introuvable.</p>
-</body>
-
-</html>
+@extends('layouts.app')
+@section('title', 'Page non trouvée')
+@section('content')
+<h1>Erreur 404</h1>
+<p>La page que vous recherchez est introuvable.</p>
+@endsection
