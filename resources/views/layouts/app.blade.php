625cb65988579a350d55843925de8d8d013ead75
KevinDaCruz <kevindc39@hotmail.com>
2026-03-10 16:54:49 +0100

    Activité 16

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Mon site Laravel')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 </head>
 
