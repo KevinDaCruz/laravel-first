731fe09a2178dac8ff6481554006c55be4d79437
KevinDaCruz <kevindc39@hotmail.com>
2026-03-10 11:34:16 +0100

    Activité 10

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,15 +7,11 @@
 </head>
 
 <body>
-    <header>
-        <h1>Mon site Laravel</h1>
-    </header>
+    @include('partials.header')
     <main>
         @yield('content')
     </main>
-    <footer>
-        <p>© 2026 - Tous droits réservés</p>
-    </footer>
+    @include('partials.footer')
 </body>
 
 </html>
