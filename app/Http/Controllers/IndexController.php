672d3346bfaa79add811695180876093aa085f0c
KevinDaCruz <kevindc39@hotmail.com>
2026-03-10 14:30:21 +0100

    Activité 12 : Rendre les articles dynamiquement via une boucle

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,6 +9,21 @@ class IndexController extends Controller
     public function index()
     {
         $name = 'Kévin';
-        return view('welcome', ['name' => $name]);
+        $articles = [
+            [
+                'title' => 'L’IA soigne mieux',
+                'description' => 'L’intelligence artificielle aide les médecins à diagnostiquer plus vite.'
+            ],
+            [
+                'title' => 'Villes vertes',
+                'description' => 'Les métropoles deviennent plus écologiques et durables.'
+            ],
+            [
+                'title' => 'Télétravail',
+                'description' => 'Plus de liberté, mais aussi plus de solitude.'
+            ]
+        ];
+
+        return view('welcome', ['name' => $name, 'articles' => $articles]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,11 @@
 <h2>Bienvenue sur la page d’accueil</h2>
 <p>Contenu principal de la page d'accueil.</p>
 
-<x-article title="L’IA soigne mieux" description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite." />
-<x-article title="Villes vertes" description="Les métropoles deviennent plus écologiques et durables." />
-<x-article title="Télétravail" description="Plus de liberté, mais aussi plus de solitude." />
+<ul>
+    @foreach ($articles as $article)
+        <li>
+            <x-article :title="$article['title']" :description="$article['description']" />
+        </li>
+    @endforeach
+</ul>
 @endsection
