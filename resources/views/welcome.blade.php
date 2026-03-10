9acc20b04a41b22f29feb4ce3acedda90f8ff3bc
KevinDaCruz <kevindc39@hotmail.com>
2026-03-10 13:47:53 +0100

    Activité 11

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,4 +5,8 @@
 @section('content')
 <h2>Bienvenue sur la page d’accueil</h2>
 <p>Contenu principal de la page d'accueil.</p>
+
+<x-article title="L’IA soigne mieux" description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite." />
+<x-article title="Villes vertes" description="Les métropoles deviennent plus écologiques et durables." />
+<x-article title="Télétravail" description="Plus de liberté, mais aussi plus de solitude." />
 @endsection
